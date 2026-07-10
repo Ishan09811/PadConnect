b56e514e990813386f0d60e839c09f563563de17
Ishan09811 <156402647+Ishan09811@users.noreply.github.com>
2026-07-10 21:15:42 +0530

    bump version to 0.6.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "io.github.padconnect"
         minSdk = 29
         targetSdk = 36
-        versionCode = 7
-        versionName = "0.6.1"
+        versionCode = 8
+        versionName = "0.6.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
